8e0f1bee62e70bfae47bb64b7ca688290bb7dc42
Artsiom Mezin <artsiom.mezin@behavox.com>
2019-04-18 12:57:40 +0100

    Some tasks are done

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,11 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override operator fun compareTo(anotherDate: MyDate): Int {
+        if (this.year != anotherDate.year) return this.year - anotherDate.year
+        if (this.month != anotherDate.month) return this.month - anotherDate.month
+        if (this.dayOfMonth != anotherDate.month) return  this.dayOfMonth - anotherDate.month
+
+        return 0
+    }
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
